9e97b82add2e475a1bba2f6a5128c80a9712a6dc
Aliaksei Lutskevich <lutskevich.alexey@gmail.com>
2022-03-01 10:35:21 +0300

    v 1.5

--- src/test/java/TestStarter.java
+++ src/test/java/TestStarter.java
@@ -2,7 +2,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -32,45 +31,19 @@ public class TestStarter {
     driver.close();
     driver.quit();
   }
-  @Disabled
+
   @Test
   @DisplayName("catalogContentMenuCheck")
   public void testCatalogContentMenu() {
     (new WebDriverWait(driver, 2)).until(ExpectedConditions.
         presenceOfElementLocated(By.xpath("//a[@href='https://catalog.onliner.by/']"))).click();
-    WebElement element = driver.findElement(By.xpath("//span[contains(text(),'Электроника')]"));
+    WebElement element = driver.findElement(
+        By.xpath("//*[@class = 'catalog-navigation-classifier ']"
+            + "//*[@class = 'catalog-navigation-classifier__item ']"));
     assertThat(element.isDisplayed()).overridingErrorMessage(()
         -> "Element not found.").isTrue();
-    WebElement element1 = driver.findElement(By.xpath("//span[contains(text(),'Компьютеры и')]"));
-    assertThat(element1.isDisplayed()).overridingErrorMessage(()
-        -> "Element not found.").isTrue();
-    WebElement element2 = driver.findElement(
-        By.xpath("//span[contains(text(),'Бытовая техника')]"));
-    assertThat(element2.isDisplayed()).overridingErrorMessage(()
-        -> "Element not found.").isTrue();
-    WebElement element3 = driver.findElement(By.xpath("//span[contains(text(),'Стройка и')]"));
-    assertThat(element3.isDisplayed()).overridingErrorMessage(()
-        -> "Element not found.").isTrue();
-    WebElement element4 = driver.findElement(By.xpath("//span[contains(text(),'Дом и')]"));
-    assertThat(element4.isDisplayed()).overridingErrorMessage(()
-        -> "Element not found.").isTrue();
-    WebElement element5 = driver.findElement(By.xpath("//span[contains(text(),'Авто и')]"));
-    assertThat(element5.isDisplayed()).overridingErrorMessage(()
-        -> "Element not found.").isTrue();
-    WebElement element6 = driver.findElement(By.xpath("//span[contains(text(),'Красота и')]"));
-    assertThat(element6.isDisplayed()).overridingErrorMessage(()
-        -> "Element not found.").isTrue();
-    WebElement element7 = driver.findElement(By.xpath("//span[contains(text(),'Детям и')]"));
-    assertThat(element7.isDisplayed()).overridingErrorMessage(()
-        -> "Element not found.").isTrue();
-    WebElement element8 = driver.findElement(By.xpath("//span[contains(text(),'Работа и')]"));
-    assertThat(element8.isDisplayed()).overridingErrorMessage(()
-        -> "Element not found.").isTrue();
-    WebElement element9 = driver.findElement(By.xpath("//span[contains(text(),'Еда')]"));
-    assertThat(element9.isDisplayed()).overridingErrorMessage(()
-        -> "Element not found.").isTrue();
   }
-  @Disabled
+
   @Test
   @DisplayName("computersAndNetworkMenuCheck")
   public void testComputersAndNetworkMenu() {
@@ -113,13 +86,21 @@ public class TestStarter {
     actions.moveToElement(link).perform();
     WebElement element = (new WebDriverWait(driver, 4)).until(ExpectedConditions.
         presenceOfElementLocated(By.xpath
-            ("//div[@class='catalog-navigation-list__dropdown']"
-                + "//span[@class='catalog-navigation-list__dropdown-title']")));
+            ("//*[@class='catalog-navigation-list__aside-title' and contains(text(),"
+                + " 'омплектующие')]//following-sibling::"
+                + "div[@class='catalog-navigation-list__dropdown']"
+                + "//a//*[contains(@class, 'title')]")));
     assertThat(element.isDisplayed()).overridingErrorMessage(()
-           -> "Element 1 not found.").isTrue();
+        -> "Element 1 not found.").isTrue();
     WebElement element1 = driver.findElement(By.xpath
-            ("//*[@class='catalog-navigation-list__dropdown-description']"));
+        ("//*[@class='catalog-navigation-list__aside-title' and contains(text(),"
+            + " 'омплектующие')]//following-sibling::"
+            + "div[@class='catalog-navigation-list__dropdown']"
+            + "//a//*[contains(@class, 'description')]"));
     assertThat(element1.isDisplayed()).overridingErrorMessage(()
-           -> "Element 2 not found.").isTrue();
+        -> "Element 2 not found.").isTrue();
   }
 }
+
+
+
